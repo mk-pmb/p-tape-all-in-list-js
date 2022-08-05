029f9192d518c85c588c1e3a6e25876de6187ab0
mk-pmb <mk-pmb@users.noreply.github.com>
2022-08-05 04:38:58 +0200

    v0.1.1: Fix deps and API (whoops.)

--- ptl.mjs
+++ ptl.mjs
@@ -6,8 +6,8 @@ import test from 'p-tape';
 
 
 const ptl = function pTapeAllInList(opt, testFuncs) {
-  if (!opt) { return ptl(true, list); }
-  if (opt.forEach) { return ptl(true, list); }
+  if (!opt) { return ptl(true, testFuncs); }
+  if (opt.forEach) { return ptl(true, testFuncs); }
   const { extraApi } = opt;
   testFuncs.forEach(f => test(f.name, t => f(Object.assign(t, extraApi))));
 };
